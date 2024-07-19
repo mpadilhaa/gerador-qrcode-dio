11b950b7f9a0dbda9c8c07089fb7f91706aa5211
mpadilhaa <mpadilhaaac@gmail.com>
2024-07-18 21:57:43 -0300

    feat: completed project

---
+++ src/services/password/handle.js
@@ -0,0 +1,35 @@
+async function handle() {
+  let characters = [];
+  let password = "";
+
+  const passwordLength = process.env.SPECIAL_CHARACTERS;
+
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  if (process.env.UPPERCASE_LETTERS === "true") {
+    characters.push(...letters);
+  }
+  if (process.env.LOWERCASE_LETTERS === "true") {
+    characters.push(...letters.toLowerCase());
+  }
+  if (process.env.NUMBERS === "true") {
+    characters.push(..."0123456789");
+  }
+  if (process.env.NUMBERS === "true") {
+    characters.push(..."0123456789");
+  }
+
+  if (process.env.SPECIAL_CHARACTERS === "true") {
+    characters.push(..."!@#$%&*-.,");
+  }
+
+  for (let i = 0; i < passwordLength; i++) {
+    const index = Math.floor(Math.random() * characters.length);
+
+    password += characters[index];
+  }
+
+  return password;
+}
+
+export default handle;
--- src/services/qr-code/create.js
+++ src/services/qr-code/create.js
@@ -1,5 +1,6 @@
 import prompt from "prompt";
 import promptQrCode from "../../prompts/prompt-qrcode.js";
+import handle from "./handle.js";
 
 async function createQrCode() {
   prompt.get(promptQrCode, handle);
